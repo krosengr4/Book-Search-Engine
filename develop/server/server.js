fffee3eef6ad30e0b328636d5644da6bed13ab90
krosengr4 <rosenkev4@gmail.com>
2024-01-17 16:00:12 -0600

    added apollo-server-express to dependencies and npm i

--- develop/server/server.js
+++ develop/server/server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleWear } = require('@apollo/server/express4');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
